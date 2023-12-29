f252e4b0a0c696aa28a070c112405fb1e80b13f5
Fabio Faria <fabiofa87@gmail.com>
2023-12-29 10:35:59 -0300

    first commit

---
+++ src/core/category/domain/entities.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+from src.core.__seedwork.domain.entities import Entity
+
+
+@dataclass(kw_only=True, frozen=True)
+class Category(Entity):
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: datetime = field(
+        default_factory=lambda: datetime.now()
+    )
